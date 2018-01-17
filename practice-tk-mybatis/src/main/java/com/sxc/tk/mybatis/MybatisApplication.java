faec5363c234d0b5bac24a741cd52b38d504faee
ZMMWMY <zhouqiming@songxiaocai.com>
2018-01-17 18:09:10 +0800

    mybatis

---
+++ practice-tk-mybatis/src/main/java/com/sxc/tk/mybatis/MybatisApplication.java
@@ -0,0 +1,30 @@
+package com.sxc.tk.mybatis;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ *
+ * @author: ZMM
+ * @version: 1.0
+ * Filename:    	MybatisApplicaiton
+ * Create at:   	2018/1/17
+ * <p>
+ * Copyright:   	Copyright (c)2018
+ * Company:     	songxiaocai
+ * <p>
+ * Modification History:
+ * Date        		      Author          Version      Description
+ * ------------------------------------------------------------------
+ * 2018/1/17    	          ZMM           1.0          1.0 Version
+ */
+@SpringBootApplication
+@MapperScan(basePackages = "com.sxc.tk.mybatis.mapper")
+public class MybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+}
---
+++ practice-tk-mybatis/src/main/java/com/sxc/tk/mybatis/conf/MainConfiguration.java
@@ -0,0 +1,23 @@
+package com.sxc.tk.mybatis.conf;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Description:
+ *
+ * @author: ZMM
+ * @version: 1.0
+ * Filename:    	MainConfiguration
+ * Create at:   	2018/1/17
+ * <p>
+ * Copyright:   	Copyright (c)2018
+ * Company:     	songxiaocai
+ * <p>
+ * Modification History:
+ * Date        		      Author          Version      Description
+ * ------------------------------------------------------------------
+ * 2018/1/17    	          ZMM           1.0          1.0 Version
+ */
+@Configuration
+public class MainConfiguration {
+}
